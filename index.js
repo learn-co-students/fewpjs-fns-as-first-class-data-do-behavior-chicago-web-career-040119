111b65a69feaed4bc66bfec77045b2d5db3677f2
Jackson Robbins <jackrobbins1@gmail.com>
2019-05-14 09:19:35 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,29 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let hour = time.substring(0, 2);
+  let fixedHour = parseInt(hour);
+  console.log(fixedHour);
+  switch (true) {
+    case (fixedHour < 12) :
+      return "Good Morning";
+      break;
+
+    case (fixedHour <= 12 || fixedHour < 17) :
+      return "Good Afternoon";
+      break;
+
+    case (fixedHour > 17) :
+      return "Good Evening";
+      break;
+
+    default:
+    return "Time entered incorectly";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  let content = document.getElementById("greeting")
+  content.innerHTML = message;
+}
